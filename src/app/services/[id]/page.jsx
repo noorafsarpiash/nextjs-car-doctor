865a81e47783eb083c2f955a947b37106af5e87b
Noor Piash <noorpiash1234@gmail.com>
2025-09-11 23:35:09 +0600

    update

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -9,16 +9,29 @@ export default async function ServicesDetailsPage({ params }) {
     const data = await servicesCollection.findOne({ _id: new ObjectId(p.id) })
     return (
         <div>
-            <section>
-                <figure className='flex justify-center'>
+            <section className='flex justify-center '>
+                <figure className='relative'>
                     <Image
                         src={"/assets/images/checkout/checkout.png"}
                         alt="Checkout"
                         width={1137}
                         height={300}
                     />
+                    <div className='transparent-layer overlay-bg absolute w-full h-full border-2 border-red-400 top-0'>
+                        <div className='w-full h-full flex font-bold text-2xl ps-16 items-center' >
+                            <div>
+                                <h1 className='text-white'>Services Details</h1>
+                            </div>
+                        </div>
+                    </div>
                 </figure>
             </section>
+
+            <section>
+                <Image src={data.img} width={400} height={280} alt={data.title} />
+                <h1 className='font-bold text-3xl'>{data.title}</h1>
+            </section>
+
             <p>{p.id}</p>
             <p>{JSON.stringify(data)}</p>
         </div>
